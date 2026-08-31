d35802960acaf5c07e13ea329556faa7536adf1e
Mikhail Tegin <ropewalker@yandex-team.ru>
2026-08-31 16:18:29 +0300

    motor config is updated

--- libecu/include/motor_config.hpp
+++ libecu/include/motor_config.hpp
@@ -17,7 +17,7 @@
 #define BLDC_NUM_POLES 40
 #define BLDC_MAX_CURRENT 18.0f
 #define BLDC_MIN_CURRENT  -6.0f
-#define BLDC_MAX_SPEED 20.0f
+#define BLDC_MAX_SPEED 12.0f
 #define BLDC_MAX_ACCELERATION 4.0f
 // Over-voltage cut-out: above this the bus is being pushed by regeneration
 // hard enough to damage the battery, and the drive goes to NEUTRAL.
